20c874c631fa9203e7ce6660326354507c55f816
AjaySelfHelp <ajaymca14326@gmail.com>
2016-05-16 23:44:24 +0530

    Add files via upload

---
+++ jecrc/jquery.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+
+$('.virtual_tour').mouseover(function(){
+ $('#d1').stop().animate({opacity:'0.4'});//opacity-drack of the image opacity
+ 
+  
+    });
+	$('.virtual_tour').mouseout(function(){
+	 $('#d1').animate({opacity:'1.0'});
+	 });
+	 
+	 
+	 
+	 $('.leadership').mouseover(function(){
+   $('#d2').stop().animate({opacity:'0.4'});//opacity-drack of the image opacity
+ 
+  
+    });
+	$('.leadership').mouseout(function(){
+	 $('#d2').animate({opacity:'1.0'});
+	 });
+	 
+	 
+	 
+	  $('.image').mouseover(function(){
+   $('#d3').stop().animate({opacity:'0.4'});//opacity-drack of the image opacity
+ 
+  
+    });
+	$('.image').mouseout(function(){
+	 $('#d3').animate({opacity:'1.0'});
+	 });
+	 
+
+
+
+
+	 
+$('.two .video').mouseover(function(){
+ $('#d4').stop().animate({opacity:'0.6'});//opacity-drack of the image opacity
+});
+
+$('.two .video').mouseout(function(){
+ $('#d4').animate({opacity:'1.0'});
+ });	 
+ 
+ 
+ 
+ 	 
+$('.two .life').mouseover(function(){
+ $('#d5').stop().animate({opacity:'0.6'});//opacity-drack of the image opacity
+});
+
+$('.two .life').mouseout(function(){
+ $('#d5').animate({opacity:'1.0'});
+ });	
+
+x
+ 
+	 
+	 
+});
